2f9c3830b6758dce0564a8589c628effcf065dcd
thomas <thomas.lemetayer.35@gmail.com>
2020-01-04 11:14:25 +0000

    up eigen

--- eigen/main.cpp
+++ eigen/main.cpp
@@ -47,8 +47,10 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
     std::cout << " * Nombre d'image de la base : " << paths.size()<< std::endl;
     std::cout << " * Nombre de U : " << m_maxEigenFace << std::endl;
 
-    vpMatrix dataBase(m_size,m_maxEigenFace);
 
+    // Creation du vpColVector pour le mean face
+    std::cout << "[INFO] Loading Reference Base" << std::endl;
+    vpMatrix dataBase(m_size,m_maxEigenFace);
     int cpt=0;
     for(auto path : paths)
     {
@@ -65,6 +67,22 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
         }
         cpt++;
     }
+
+    // Creation du vpColVector pour le mean face
+    std::cout << "[INFO] Loading Test Base" << std::endl;
+    vpMatrix dataBase_Test(m_size,40);
+    for (size_t idx = 0; idx < 40; idx++)
+    {
+        std::string path = "../Donnees/att_faces/s"+std::to_string(idx+1)+"/10.pgm";
+        vpImageIo::read(I,path);
+        for (int i = 0; i < m_h; i++)
+        {
+            for (int j = 0; j < m_w; j++)
+            {
+                dataBase_Test[i*m_w+j][idx] = I[i][j]/255.;
+            }
+        }
+    }
     
     // Creation du vpColVector pour le mean face
     std::cout << "[INFO] Creation Mean images ...." << std::endl;
@@ -73,7 +91,7 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
 
     writeEigenFaceInImage(m_w,m_h,"../result/mean.jpg",m_vMean);
     srand(time(0));  
-    for (int i = 0; i < nbTirage; i++)
+    for (int i = 0; i < 4; i++)
     {
         int elem = rand()%m_maxEigenFace;
         vpColVector face = dataBase.getCol(elem);
@@ -96,22 +114,30 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
         }
     }
 
+    // SVD
+    std::cout << "[INFO] Calcul de U ... " << std::endl;
     vpMatrix v;
     vpColVector eigenValue;
     vpMatrix eigenVector=A;
     eigenVector.svd(eigenValue,v);
-    for (int j = 0; j < nbTirage; j++)
+
+    std::cout << "[INFO] Sauvegarde des 4 premières eigenfaces " << std::endl;
+    for (int j = 0; j < 4; j++)
     {
         vpColVector eigenface = eigenVector.getCol(j);
         std::string save_path = "../result/eigen_face_"+std::to_string(eigenValue[j])+".jpg";
         writeEigenFaceInImage(m_w,m_h,save_path,eigenface);
     }
 
-    for (int j = 0; j < nbTirage; j++)
-    {
-        int elem = rand()%m_maxEigenFace;
-        vpColVector face = dataBase.getCol(elem);
+    std::cout << "[INFO] Reprojection d'une image de reference pour K=50,150 et 360 " << std::endl;
+    int K_values[3] = {50,150,360};
 
+    int elem = rand()%m_maxEigenFace; // Choix d'un visage aléatoire
+    vpColVector face = dataBase.getCol(elem);
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        K = K_values[i];
         vpColVector W(K);
         for (size_t k = 0; k < K; k++)
         {
@@ -126,30 +152,67 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
         reproj_I+=m_vMean;
 
         double error = (face-reproj_I).infinityNorm();
-        std::cout << error << std::endl;
+        std::cout << "K="<<K<<" ; Error=" << error << std::endl;
 
-        std::string save_path = "../result/reproj_"+std::to_string(j)+".jpg";
+        std::string save_path = "../result/reproj_ref_"+std::to_string(K)+".jpg";
         writeEigenFaceInImage(m_w,m_h,save_path,reproj_I);
     }
 
-    std::cout << "[INFO] Calcul Normalized Cumulative EigenValue " << std::endl;
+    std::cout << "[INFO] Reprojection d'une image de test pour K=50,150 et 360 " << std::endl;
+    elem = elem/9; // Visage de la même personne pour le test
+    face = dataBase_Test.getCol(elem);
 
-    double sum_eigenvalue = eigenValue.sum();
-    vpColVector cumule_eigenvalue = eigenValue;
-    for (size_t i = 0; i < cumule_eigenvalue.size(); i++)
+    for (size_t i = 0; i < 3; i++)
     {
-        if(i==0){
-            cumule_eigenvalue[i] = eigenValue[i] / sum_eigenvalue;
-            continue;
-        }else{
-            cumule_eigenvalue[i] = cumule_eigenvalue[i-1] + eigenValue[i] / sum_eigenvalue;
+        K = K_values[i];
+        vpColVector W(K);
+        for (size_t k = 0; k < K; k++)
+        {
+            W[k]=eigenVector.getCol(k)*(face-m_vMean);
         }
-        std::cout << cumule_eigenvalue[i] << std::endl;
+
+        vpColVector reproj_I = vpColVector(m_size);
+        for (size_t k = 0; k < K; k++)
+        {
+            reproj_I+=W[k]*eigenVector.getCol(k);
+        }
+        reproj_I+=m_vMean;
+
+        double error = (face-reproj_I).infinityNorm();
+        std::cout << "K="<<K<<" ; Error=" << error << std::endl;
+
+        std::string save_path = "../result/reproj_test_"+std::to_string(K)+".jpg";
+        writeEigenFaceInImage(m_w,m_h,save_path,reproj_I);
     }
 
-    std::cout << "[INFO] Error reconstruction " << std::endl;
 
-/*
+    std::cout << "[INFO] Calcul Normalized Cumulative EigenValue " << std::endl;
+    std::cout << "Display result ? (Y/N)"<<std::endl;
+    std::string display;
+    std::cin >> display;
+
+    if(display=="Y"){
+        double sum_eigenvalue = eigenValue.sum();
+        vpColVector cumule_eigenvalue = eigenValue;
+        for (size_t i = 0; i < cumule_eigenvalue.size(); i++)
+        {
+            if(i==0){
+                cumule_eigenvalue[i] = eigenValue[i] / sum_eigenvalue;
+                continue;
+            }else{
+                cumule_eigenvalue[i] = cumule_eigenvalue[i-1] + eigenValue[i] / sum_eigenvalue;
+            }
+            std::cout << cumule_eigenvalue[i] << std::endl;
+        }
+    }
+
+
+    std::cout << "[INFO] Error reconstruction " << std::endl;
+    std::cout << "Display result ? (Y/N)"<<std::endl;
+    std::cin >> display;
+
+    if(display=="Y"){
+
     for (size_t nbEigen = 0; nbEigen < m_maxEigenFace; nbEigen++)
     {
         double error=0;
@@ -179,9 +242,9 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
         error/=m_maxEigenFace;
         std::cout << error << std::endl;
     }
-*/
-    std::cout << "[INFO] Identification " << std::endl;
+    }
 
+    std::cout << "[INFO] Compute Error Matrix for K=20 " << std::endl;
     int K_value=20;
 
     vpMatrix ErrorMatrix(m_maxEigenFace,m_maxEigenFace);
@@ -211,12 +274,11 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
 
 #pragma omp critical
             ErrorMatrix[i][j] = error;
-            //std::cout<<i<<" "<<j<<" "<<error<<std::endl;
         }
     }
 
     std::ofstream myfile;
-    myfile.open ("matrixError.txt");
+    myfile.open ("../result/matrixError.txt");
     myfile<<ErrorMatrix;
     myfile.close();
 
@@ -224,6 +286,7 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
     double max_same = 0;
     double min_diff = 50000;
     double max_diff = 0;
+
     for (size_t i = 0; i < m_maxEigenFace; i++)
     {
         for (size_t j = 0; j < m_maxEigenFace; j++)
@@ -244,79 +307,13 @@ void buildBDFaces(const std::vector<std::string>& paths, int maxEigenFace)
     std::cout << "Min Diff : "<<min_diff << std::endl;
     std::cout << "Max Diff : "<<max_diff << std::endl;
 
-    std::cout << "[INFO] Load Base Test " << std::endl;
-
     double theta = (max_same+min_diff)/2;
+    std::cout << "Theta : "<<theta<<std::endl;
 
-    vpMatrix dataBase_Test(m_size,40);
-    for (size_t idx = 0; idx < 40; idx++)
-    {
-        std::string path = "../Donnees/att_faces/s"+std::to_string(idx+1)+"/10.pgm";
-        vpImageIo::read(I,path);
-        for (int i = 0; i < m_h; i++)
-        {
-            for (int j = 0; j < m_w; j++)
-            {
-                dataBase_Test[i*m_w+j][idx] = I[i][j]/255.;
-            }
-        }
-    }
+    std::cout << "[INFO] Reconnaissance facial en fonction de K " << std::endl;
 
-    vpMatrix corr(2,2);
-
-#pragma omp parallel for
-    for (size_t k = 0; k < m_maxEigenFace; k++)
-    {
-        for (size_t i = 0; i < 40; i++)
-        {
-            vpColVector face_i = dataBase_Test.getCol(i);
-
-            vpColVector W_i(K_value);
-            for (size_t k = 0; k < K_value; k++)
-            {
-                W_i[k]=eigenVector.getCol(k)*(face_i-m_vMean);
-            }
-
-            for (size_t j = 0; j < m_maxEigenFace; i++)
-            {
-                vpColVector face_j = dataBase.getCol(j);
-
-                vpColVector W_j(K_value);
-                for (size_t k = 0; k < K_value; k++)
-                {
-                    W_j[k]=eigenVector.getCol(k)*(face_j-m_vMean);
-                }
-        
-                double error = (W_i - W_j).infinityNorm();
-
-                bool same_Ref = j/9 == i;
-                bool same_Test = error<theta;
-
-#pragma omp critical
-                {
-                    if(same_Ref&same_Test){
-                        corr[0][0]+=1;
-                    }
-                    if(!same_Ref&!same_Test){
-                        corr[1][1]+=1;
-                    }
-                    if(!same_Ref&same_Test){
-                        corr[0][1]+=1;
-                    }
-                    if(same_Ref&!same_Test){
-                        corr[1][0]+=1;
-                    }
-                }
-
-            }
-        }
-    }
-    std::cout<<corr<<std::endl;
-
-    std::cout << "[INFO] Fin calcul BD ... " << std::endl;
+    std::cout << "[INFO] Fin du programme" << std::endl;
 }
-
-
 
 std::vector<std::string> buildPathImagesAttFaces()
 {
@@ -335,7 +332,6 @@ int main()
 {
 	std::cout << "[INFO] Construction du path ..." << std::endl;
 	std::vector<std::string> paths = buildPathImagesAttFaces();
-	std::cout << "[INFO] Creation de base de donnees ..." << std::endl;
 	
     buildBDFaces(paths,400);
 	
